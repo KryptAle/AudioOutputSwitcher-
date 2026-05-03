7b90a9d8c8d57dfd610e0e02106641f538440618
KryptAle <112360932+KryptAle@users.noreply.github.com>
2026-05-03 12:44:39 -0400

    commit & push

---
+++ app/src/main/java/br/com/wasystems/audiooutputswitcher/SwitcherShortcutActivity.kt
@@ -0,0 +1,17 @@
+package com.weslley75.audiooutputswitcher // Usa el paquete oficial del repo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.weslley75.audiooutputswitcher.utils.AudioOutputFallback
+
+class SwitcherShortcutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Abre el selector nativo usando la lógica de respaldo del proyecto
+        AudioOutputFallback.openSelector(this)
+
+        // Cierra la actividad inmediatamente para que sea invisible al usuario
+        finish()
+    }
+}
